c24a40a0ba01f2ede469deb8af4317d73ed53b92
VITOR YUZO TAKEI <vitor.yuzo.profissional@gmail.com>
2025-09-05 18:43:08 -0300

    cadastro 2
    Necessaria alteraçoes de estilo

--- password/src/app/app.ts
+++ password/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './views/home/home';
 import { Cadastro1 } from './views/cadastro-1/cadastro-1';
+import { Cadastro2 } from './views/cadastro-2/cadastro-2';
 
 @Component({
   // selector: 'app-root',
@@ -9,10 +10,16 @@ import { Cadastro1 } from './views/cadastro-1/cadastro-1';
   // templateUrl: './app.html',
   // styleUrl: './app.scss'
 
+//   selector: 'app-root',
+//   imports: [Cadastro1],
+//   templateUrl: './app.html',
+//   styleUrl: './app.scss'
+
   selector: 'app-root',
-  imports: [Cadastro1],
+  imports: [Cadastro2],
   templateUrl: './app.html',
   styleUrl: './app.scss'
+
 })
 export class App {
   protected readonly title = signal('password');
