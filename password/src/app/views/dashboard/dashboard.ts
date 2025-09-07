bb4d17a85c21c92c6e7e30cd28be8c3fecb7a328
Pedro22D0W <pedro.rodolfo.galvao22@hotmail.com>
2025-09-07 16:44:01 -0300

    senhas renderizadas

--- password/src/app/views/dashboard/dashboard.ts
+++ password/src/app/views/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import {PasswordService } from '../../services/password-api.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -11,7 +11,8 @@ import { CommonModule } from '@angular/common';
 })
 export class Dashboard implements OnInit {
       userPasswords: any[] = [];
-    constructor(private router: Router,private passwordService: PasswordService){}
+      loading = true;
+    constructor(private cd: ChangeDetectorRef,private router: Router,private passwordService: PasswordService){}
       ngOnInit(): void {
     this.loadPasswords();
   }
@@ -19,24 +20,30 @@ export class Dashboard implements OnInit {
     this.passwordService.getPasswords().subscribe({
       next: (res) => {
         this.userPasswords = res;
+        this.loading = false;
+        this.cd.detectChanges();
         console.log('Dados recebidos:', this.userPasswords);
       },
       error: (err) => {
         console.error('Erro ao carregar dados:', err);
+        this.loading = false;
       }
     });
   }
   copyPassword(password: string) {
   if (!password) return;
-
-  // Copia para a área de transferência
-  navigator.clipboard.writeText(password).then(() => {
-    console.log('Senha copiada com sucesso!');
-    // Você pode exibir um toast ou alert se quiser
-  }).catch(err => {
-    console.error('Erro ao copiar a senha: ', err);
-  });
+  // usa a API de Clipboard
+  navigator.clipboard.writeText(password).then(
+    () => {
+      console.log('Senha copiada para a área de transferência!');
+      // opcional: mostrar uma notificação para o usuário
+    },
+    (err) => {
+      console.error('Erro ao copiar senha: ', err);
+    }
+  );
 }
+
 
 logout() {
   // Remove o token do localStorage
